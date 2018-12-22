97ccc75bf688c7b94702c1f9db579c1eb10e0345
OiBRUV <45884936+OiBRUV@users.noreply.github.com>
2018-12-22 10:17:45 -0500

    Add files via upload

--- teamcode/Atonomus.java
+++ teamcode/Atonomus.java
@@ -1,33 +1,6 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-package org.firstinspires.ftc.teamcode;
+
+package org.firstinspires.ftc.TeamCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -49,6 +22,7 @@ public class Atonomus extends LinearOpMode {
     private DcMotor rightMotor = null;
     private DcMotor ArmMotor = null;
     private Servo servom = null;
+    private Servo dropper = null;
 
     //@Override
     /*public void init() {
@@ -56,45 +30,87 @@ public class Atonomus extends LinearOpMode {
         rightMotor = hardwareMap.get(DcMotor.class, "Right_Motor");
         ArmMotor = hardwareMap.get(DcMotor.class, "Arm_Motor");
         }*/
-        @Override
-        public void runOpMode() throws InterruptedException{
-            leftMotor  = hardwareMap.get(DcMotor.class, "Left_Motor");
-            rightMotor = hardwareMap.get(DcMotor.class, "Right_Motor");
-            ArmMotor = hardwareMap.get(DcMotor.class, "Arm_Motor");
-            rightMotor.setDirection(DcMotor.Direction.REVERSE);
-            servom = hardwareMap.servo.get("latch");
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftMotor = hardwareMap.get(DcMotor.class, "Left_Motor");
+        rightMotor = hardwareMap.get(DcMotor.class, "Right_Motor");
+        ArmMotor = hardwareMap.get(DcMotor.class, "Arm_Motor");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        servom = hardwareMap.servo.get("latch");
 
-            telemetry.addData("Mode", "waiting");
-            telemetry.update();
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
 
-            // wait for start button.
+        // wait for start button.
 
-            //waitForStart();
+        //waitForStart();
 
-            telemetry.addData("Mode", "running");
-            telemetry.update();
+        telemetry.addData("Mode", "running");
+        telemetry.update();
 
-            // set both motors to 25% power.
+        // set both motors to 25% power.
 
-            ArmMotor.setPower(-.5);
+        ArmMotor.setPower(-.5);
 
-            // wait for 2 seconds.
-            sleep(900);
+        // wait for 800 milliseconds.
+        sleep(800);
 
-            // set motor power to zero to stop motors.
+        // set motor power to zero to stop motors.
 
-            servom.setPosition(1);
-            servom.setPosition(0);
+        servom.setPosition(1);
+        servom.setPosition(0);
 
-            sleep(1500);
+        sleep(1500);
 
-            leftMotor.setPower(-1);
-            rightMotor.setPower(-1);
+        leftMotor.setPower(-1);
+        rightMotor.setPower(-1);
 
-            sleep(1000);
+        sleep(1000);
 
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
+        leftMotor.setPower(-1);
+        rightMotor.setPower(1);
 
+        sleep(1000);
+
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+        boolean forward = false;
+        boolean left = false;
+        boolean right = false;
+        boolean img = false;
+
+        for (int i=0; i==2; i++) {
+
+            if (img) {
+                rightMotor.setPower(-.5);
+                leftMotor.setPower(-.5);
+                sleep(1000);
+                rightMotor.setPower(0);
+                leftMotor.setPower(0);
+            } else if (!img) {
+                rightMotor.setPower(-.5);
+                leftMotor.setPower(.5);
+                sleep(1000);
+                rightMotor.setPower(0);
+                leftMotor.setPower(0);
+            } else if (!img){
+                rightMotor.setPower(.5);
+                leftMotor.setPower(-.5);
+                sleep(2000);
+                rightMotor.setPower(0);
+                leftMotor.setPower(0);
+            }
+            else if (!img) {
+                rightMotor.setPower(-.5);
+                leftMotor.setPower(.5);
+                sleep(1000);
+                rightMotor.setPower(0);
+                leftMotor.setPower(0);
+            }
+            if(i==2){
+                img=true;
+            }
+        }
     }
 }
