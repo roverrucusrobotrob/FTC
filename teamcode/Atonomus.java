58c336560d6351a42cec2212f21249db4354df46
OiBRUV <45884936+OiBRUV@users.noreply.github.com>
2019-01-05 14:13:47 -0500

    Update Atonomus.java

--- teamcode/Atonomus.java
+++ teamcode/Atonomus.java
@@ -17,6 +17,10 @@ import static java.lang.Thread.*;
 public class Atonomus extends LinearOpMode {
 
     // Declare OpMode members.
+    private double ticksPerRevolution = 1440;
+    private double diameter = 2.5;
+    private double gearReduction = .5;
+    private double ticksPerInch = (ticksPerRevolution * gearReduction) / diameter * 3.1415;
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftMotor = null;
     private DcMotor rightMotor = null;
@@ -30,6 +34,15 @@ public class Atonomus extends LinearOpMode {
         rightMotor = hardwareMap.get(DcMotor.class, "Right_Motor");
         ArmMotor = hardwareMap.get(DcMotor.class, "Arm_Motor");
         }*/
+    public void moveLift(double power,double inches, long time) throws InterruptedException {
+        ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        ArmMotor.setTargetPosition((int) (ticksPerInch * inches));
+        ArmMotor.setPower(power);
+        if (inches >= 12) {
+            ArmMotor.setPower(0);
+        }
+
+    }
     @Override
     public void runOpMode() throws InterruptedException {
         leftMotor = hardwareMap.get(DcMotor.class, "Left_Motor");
@@ -37,6 +50,8 @@ public class Atonomus extends LinearOpMode {
         ArmMotor = hardwareMap.get(DcMotor.class, "Arm_Motor");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
         servom = hardwareMap.servo.get("latch");
+        ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addData("Mode", "waiting");
         telemetry.update();
@@ -49,14 +64,19 @@ public class Atonomus extends LinearOpMode {
         telemetry.update();
 
         // set both motors to 25% power.
-
+        moveLift(.5, 12, 5);
         ArmMotor.setPower(-.5);
-
+        //or naw
         // wait for 800 milliseconds.
         sleep(800);
 
-        // set motor power to zero to stop motors.
+        // set motor power to zero to stop motors. We Forgot this so um yeah
+        ArmMotor.setPower(0);
 
+        //Wait some milli-sss u catchin’ my vibes
+        sleep(800);
+
+        //set servo from 1 to 0
         servom.setPosition(1);
         servom.setPosition(0);
 
@@ -64,11 +84,6 @@ public class Atonomus extends LinearOpMode {
 
         leftMotor.setPower(-1);
         rightMotor.setPower(-1);
-
-        sleep(1000);
-
-        leftMotor.setPower(-1);
-        rightMotor.setPower(1);
 
         sleep(1000);
 
@@ -88,7 +103,33 @@ public class Atonomus extends LinearOpMode {
                 sleep(1000);
                 rightMotor.setPower(0);
                 leftMotor.setPower(0);
-            } else if (!img) {
+                if(img){
+                    rightMotor.setPower(.5);
+                    leftMotor.setPower(.5);
+                    sleep(1000);
+                    rightMotor.setPower(0);
+                    leftMotor.setPower(0);
+                    sleep(800);
+                    dropper.setPosition(0);
+                    sleep(500);
+                    dropper.setPosition(1);
+                }
+                if(!img){
+                    rightMotor.setPower(-.5);
+                    leftMotor.setPower(.5);
+                    sleep(1000);
+                    rightMotor.setPower(0);
+                    leftMotor.setPower(0);
+                }
+                else if(!img){
+                    rightMotor.setPower(.5);
+                    leftMotor.setPower(-.5);
+                    sleep(2000);
+                    rightMotor.setPower(0);
+                    leftMotor.setPower(0);
+                }
+            }
+            if (!img) {
                 rightMotor.setPower(-.5);
                 leftMotor.setPower(.5);
                 sleep(1000);
@@ -101,7 +142,7 @@ public class Atonomus extends LinearOpMode {
                 rightMotor.setPower(0);
                 leftMotor.setPower(0);
             }
-            else if (!img) {
+            else if (!img){
                 rightMotor.setPower(-.5);
                 leftMotor.setPower(.5);
                 sleep(1000);
@@ -114,3 +155,5 @@ public class Atonomus extends LinearOpMode {
         }
     }
 }
+
+
